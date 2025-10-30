1da34726c03a1cdeb188ebfa41aae53d79e9d36e
Haru Kato <s24C1040WJ@s.chibakoudai.jp>
2025-10-30 13:06:29 +0900

    change2

--- test.bash
+++ test.bash
@@ -6,8 +6,17 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 加藤 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo　あ | ./plus)
+[ "$?" = 1 ]    || ng "LINENO"
+[ "${out}" = ""]|| ng "LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]  || ng "$LINENO"
+[ "${out}" = ""] || ng "LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
