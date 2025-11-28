7de9e1ad9be55d762e207ff7e79a6c0b20ec27a4
Haru Kato <s24C1040WJ@s.chibakoudai.jp>
2025-11-28 16:16:14 +0900

    1st homework

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 C-joker
+# SPDX-License-Identifier* BSD-3-Clause
 
 ng () {
     echo "${1}行目が違うよ"
@@ -8,16 +10,16 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
+out=$(seq 5 | ./main.py)
 [ "${out}" = 15 ] || ng "$LINENO"
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
+out=$(echo あ | ./main.py)
 [ "$?" = 1 ] || ng "$LINENO"
 [ -z "${out}" ] || ng "$LINENO"
 
 ### EMPTY INPUT ###
-out=$(echo | ./plus)
+out=$(echo | ./main.py)
 [ "$?" = 1 ] || ng "$LINENO"
 [ -z "${out}" ] || ng "$LINENO"
 
