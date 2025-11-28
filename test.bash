142fce9cada4ea6acae6af9176b08eb69ac25342
Haru Kato <s24C1040WJ@s.chibakoudai.jp>
2025-11-28 15:01:25 +0900

    1sthomework

--- test.bash
+++ test.bash
@@ -1,10 +1,25 @@
-import sys
+#!/bin/bash -xv
+# SPDX ...
 
-def main():
-    text = sys.stdin.read().strip().lower()
-    vowels = "aeiou"
-    count = sum(1 for c in text if c in vowels)
-    print(count)
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
 
-if __name__ == "__main__":
-    main()
+res=0
+
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1]       || ng "$LINENO"
+[ "${out}" = ""]  || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1]       || ng "$LINENO"
+[ "${out}" = ""]  || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
